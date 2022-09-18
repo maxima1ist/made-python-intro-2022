f7fe1cf9e3291d6f8190a13ff461eced760533de
maxima1ist <mksstepura@gmail.com>
2022-09-18 17:28:02 +0300

    Fix review

---
+++ 01/tests.py
@@ -0,0 +1,32 @@
+import utils as my_math
+
+# unit tests for 'solve_quadratic' function
+try:
+    my_math.solve_quadratic(0, 0, 0) == None
+    assert False
+except:
+    pass
+
+assert my_math.solve_quadratic(1, 2, 3) == None
+
+answer = my_math.solve_quadratic(1, 1, 0.25)
+assert len(answer) == 2
+assert my_math.is_close(answer[0], -0.5)
+assert my_math.is_close(answer[1], -0.5)
+
+answer = my_math.solve_quadratic(3, 10, 5)
+assert len(answer) == 2
+assert my_math.is_close(answer[0], -2.72076, 1e-5)
+assert my_math.is_close(answer[1], -0.61257, 1e-5)
+
+# unit tests for 'split_by_even' function
+assert my_math.split_by_even([]) == ([], [])
+assert my_math.split_by_even([1]) == ([], [1])
+assert my_math.split_by_even([2]) == ([2], [])
+assert my_math.split_by_even([1, 2, 3, 4, 5, 6]) == ([2, 4, 6], [1, 3, 5])
+
+arr = [value for value in range(-10000, 10000)]
+assert my_math.split_by_even(arr) == ([value for value in arr if value % 2 == 0],
+                                      [value for value in arr if value % 2 == 1])
+
+print("All tests passed!")
--- 01/utils.py
+++ 01/utils.py
@@ -8,16 +8,13 @@ def is_close(lhs: float, rhs: float, accuracy: float = ROUNDING_ACCURACY) -> boo
 def solve_quadratic(a: float, b: float, c: float) -> tuple:
     if is_close(a, 0):
         raise ValueError("Incorrect quadratic equation")
-    
-    D = b ** 2 - 4 * a * c
-    if D < 0:
+
+    d = b ** 2 - 4 * a * c
+    if d < 0:
         return None
-    
-    if is_close(D, 0):
-        return (-b / (2 * a),)
-    
-    D = D ** (1 / 2)
-    return ((-b - D) / (2 * a), (-b + D) / (2 * a))
+
+    d = d ** (1 / 2)
+    return ((-b - d) / (2 * a), (-b + d) / (2 * a))
 
 
 def split_by_even(arr: list) -> tuple:
@@ -28,5 +25,5 @@ def split_by_even(arr: list) -> tuple:
             even.append(value)
         else:
             not_even.append(value)
-            
+
     return (even, not_even)
